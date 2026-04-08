db6c103ee7ef6fdf4920417c479f0bf0317777e0
Vrumugun <wisniewski1@gmail.com>
2026-04-08 08:36:11 +0200

    add array and record

--- Ada_2005/simple_concepts/src/simple_concepts.adb
+++ Ada_2005/simple_concepts/src/simple_concepts.adb
@@ -1,6 +1,28 @@
 with Test_package;
+with Ada.Text_IO;
 
 procedure Simple_Concepts is
+   The_Array : constant array (1 .. 10) of Integer :=
+      (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+   Next : array (1 .. 10) of Integer;
+   My_Buffer : Test_package.buffer;
 begin
-      Test_package.Print_Root;
+   --  Test record type
+   My_Buffer.data (1 .. 11) := "Hello World";
+   My_Buffer.length := 11;
+   Ada.Text_IO.Put_Line ("First 5 letters of Buffer: " & My_Buffer.data (1 .. 5));
+
+   --  Test array type
+   for I in The_Array'Range loop
+      Next (I) := The_Array (I) + 1;
+   end loop;
+
+   --  Print the next array
+   Ada.Text_IO.Put_Line ("Next array:");
+   for I in Next'Range loop
+      Ada.Text_IO.Put (Integer'Image (Next (I)) & " ");
+   end loop;
+   Ada.Text_IO.New_Line;
+
+   Test_package.Print_Root;
 end Simple_Concepts;
--- Ada_2005/simple_concepts/src/test_package.ads
+++ Ada_2005/simple_concepts/src/test_package.ads
@@ -2,4 +2,10 @@ package Test_package is
 
    procedure Print_Root;
 
+   type buffer
+      is record
+         data : String (1 .. 100);
+         length : Natural := 0;
+      end record;
+
 end Test_package;
