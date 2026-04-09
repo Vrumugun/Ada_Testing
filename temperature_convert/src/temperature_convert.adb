47c639512ba0f9c395307fb02b46a9ea8969e0e8
Vrumugun <wisniewski1@gmail.com>
2026-04-09 09:17:24 +0200

    add type for mode selection

--- temperature_convert/src/temperature_convert.adb
+++ temperature_convert/src/temperature_convert.adb
@@ -6,11 +6,13 @@ with Ada.IO_Exceptions;
 procedure Temperature_Convert is
    type Temperature_C is delta 0.01 range -273.15 .. 1000.0;
    type Temperature_F is delta 0.01 range -1000.00 .. 1000.0;
+   type Mode_Type is (Mode_None, Mode_C_to_F, Mode_F_to_C, Mode_Exit);
 
    input_temperature_C : Temperature_C;
    input_temperature_F : Temperature_F;
    input_value : Float;
-   mode_select : Integer := 0;
+   mode_input : Integer;
+   mode_select : Mode_Type := Mode_None;
 
    function Celsius_To_Fahrenheit (C : Temperature_C) return Temperature_F is
    begin
@@ -22,20 +24,32 @@ procedure Temperature_Convert is
       return Temperature_C ((Float (F) - 32.0) * 5.0 / 9.0);
    end Fahrenheit_To_Celsius;
 begin
-   while mode_select /= 3 loop
+   while mode_select /= Mode_Exit loop
       Ada.Text_IO.Put ("Select: (1) - Convert from celsius to fahrenheit, " &
          "(2) - Convert from fahrenheit to celsius. " &
          "(3) - Exit. Enter selection: ");
       begin
-         Ada.Integer_Text_IO.Get (mode_select);
+         Ada.Integer_Text_IO.Get (mode_input);
+
+         case mode_input is
+            when 1 =>
+               mode_select := Mode_C_to_F;
+            when 2 =>
+               mode_select := Mode_F_to_C;
+            when 3 =>
+               mode_select := Mode_Exit;
+            when others =>
+               Ada.Text_IO.Put_Line ("Invalid input, not a valid mode value.");
+               mode_select := Mode_None;
+         end case;
       exception
          when Ada.IO_Exceptions.Data_Error =>
-            Ada.Text_IO.Put_Line ("Invalid input, not a valid mode values.");
+            Ada.Text_IO.Put_Line ("Invalid input, not a valid mode value.");
             Ada.Text_IO.Skip_Line;
-            mode_select := 0;
+            mode_select := Mode_None;
       end;
 
-      if mode_select = 1 then
+      if mode_select = Mode_C_to_F then
          Ada.Text_IO.Put ("Enter temperature in Celsius: ");
          begin
             Ada.Float_Text_IO.Get (input_value);
@@ -51,7 +65,7 @@ begin
                   "temperature out of range!");
                Ada.Text_IO.Skip_Line;
          end;
-      elsif mode_select = 2 then
+      elsif mode_select = Mode_F_to_C then
          Ada.Text_IO.Put ("Enter temperature in Fahrenheit: ");
          begin
             Ada.Float_Text_IO.Get (input_value);
